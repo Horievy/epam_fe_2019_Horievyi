6714daba4211ba5f9a261907e7bc6f3991066f14
Oleksandr_Horievyi <oleksandr_horievyi@epam.com>
2020-01-17 21:37:07 +0200

    Temporary changes for another computer

--- js/slider.js
+++ js/slider.js
@@ -1,58 +1,69 @@
-function Slider() {
-  this.speed = 1500;
+function Slider(config) {
+  const {sliderWrapper, time} = config;
+  this.slider = sliderWrapper.querySelector('.testimonials__slider');
+  this.arrowNext = sliderWrapper.querySelector('.testimonials__button-right');
+  this.arrowPrev = sliderWrapper.querySelector('.testimonials__button-left');
+
+  this.speed = time || 1500;
   this.next = () => this.slider.appendChild(this.slider.firstElementChild);
   this.prev = () => this.slider.prepend(this.slider.lastElementChild);
   this.arrowNext.addEventListener('click', this.next);
   this.arrowPrev.addEventListener('click', this.prev);
   this.interval = () => setInterval(() => this.next(), this.speed);
+  this.timer = this.interval();
 
-  this.sliderWrapper.addEventListener('mouseleave', () => {
+  sliderWrapper.addEventListener('mouseleave', () => {
     this.timer = this.interval();
   });
 
-  this.sliderWrapper.addEventListener('mouseenter', () => {
+  sliderWrapper.addEventListener('mouseenter', () => {
     clearInterval(this.timer);
   });
 }
 
-function TestimonalsSlider() {
-  this.slider = document.querySelector('.testimonials__slider');
-  this.sliderWrapper = document.querySelector('.testimonials__person-wrap');
-  this.arrowNext = document.querySelector('.testimonials__button-right');
-  this.arrowPrev = document.querySelector('.testimonials__button-left');
+const testimonalsSlider1 = {
+  sliderWrapper: document.querySelector('.testimonials__person-wrap'),
+  time: 2500,
+  state: 'off'
+};
 
-  Slider.call(this);
-  this.autoplaySpeed = (speed) => {
-    this.speed = speed;
-    this.interval();
-  };
+// const test = new Slider(testimonalsSlider1);
+//
+// function TestimonalsSlider() {
+//   this.slider = document.querySelector('.testimonials__slider');
+//   this.sliderWrapper = document.querySelector('.testimonials__person-wrap');
+//   this.arrowNext = document.querySelector('.testimonials__button-right');
+//   this.arrowPrev = document.querySelector('.testimonials__button-left');
+//
+//   Slider.call(this);
+//   this.autoplaySpeed = (speed) => {
+//     this.speed = speed;
+//     this.interval();
+//   };
+//
+//   this.transitionSpeed = (speed) => {
+//     [].forEach.call(this.slider.children, (item) => {
+//       item.style.transition = speed;
+//     });
+//   };
+// }
 
-  this.transitionSpeed = (speed) => {
-    [].forEach.call(this.slider.children, (item) => {
-      item.style.transition = speed;
-    });
-  };
-}
-
-function PortfolioSlider() {
-  this.slider = document.querySelector('.portfolio__items-wrap');
-  this.sliderWrapper = document.querySelector('.portfolio__items');
-  this.arrowNext = document.querySelector('.menu-btn.menu-btn--right');
-  this.arrowPrev = document.querySelector('.menu-btn');
-
-  Slider.call(this);
-  this.autoPlay = (state = 'on') => {
-    if (state === 'on') {
-      this.timer = this.interval();
-    } else if (state === 'off') {
-      this.sliderWrapper.addEventListener('mouseleave', () => {
-        clearInterval(this.timer);
-      });
-      clearInterval(this.timer);
-    } else {
-      throw new Error('Wrong input data');
-    }
-  };
+function PortfolioSlider(config) {
+  const {state} = config;
+  Slider.call(this, config);
+  // this.autoPlay = (state = 'on') => {
+  //   if (state === 'on') {
+  //     this.timer = this.interval();
+  //   } else if (state === 'off') {
+  //     this.sliderWrapper.addEventListener('mouseleave', () => {
+  //       clearInterval(this.timer);
+  //     });
+  //     clearInterval(this.timer);
+  //   } else {
+  //     throw new Error('Wrong input data');
+  //   }
+  // };
+  // this.autoPlay(state);
 
   this.showButtons = (state = 'on') => {
     if (state === 'on') {
@@ -65,12 +76,13 @@ function PortfolioSlider() {
       throw new Error('Wrong input data');
     }
   };
+  this.showButtons(state);
 }
 
-const portfolioSlider = new PortfolioSlider();
-portfolioSlider.autoPlay('on');
-portfolioSlider.showButtons('off');
-const testimonalsSlider = new TestimonalsSlider();
-testimonalsSlider.transitionSpeed('2s');
-testimonalsSlider.autoplaySpeed(3000);
-
+const portfolioSlider = new PortfolioSlider(testimonalsSlider1);
+// portfolioSlider.autoPlay('on');
+// portfolioSlider.showButtons('off');
+// const testimonalsSlider = new TestimonalsSlider();
+// testimonalsSlider.transitionSpeed('2s');
+// testimonalsSlider.autoplaySpeed(3000);
+//
