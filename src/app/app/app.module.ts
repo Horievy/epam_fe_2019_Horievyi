94d9b2b8d5c5b8a08b79121c9065aac3ad75776b
Oleksandr_Horievyi <oleksandr_horievyi@epam.com>
2020-02-27 16:34:10 +0200

    HW30-Angular-intro - Refactoring

--- src/app/app/app.module.ts
+++ src/app/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './containers/app/app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import {AppRoutingModule} from './app.routing-module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
